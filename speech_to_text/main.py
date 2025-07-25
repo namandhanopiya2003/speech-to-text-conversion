6f661f82592361d5c7f01fb207f5ed6e83e80466
Naman Dhanopiya <iamnamandhanopiya@gmail.com>
2025-07-25 19:16:26 +0530

    Cleaned main.py code

--- speech_to_text/main.py
+++ speech_to_text/main.py
@@ -11,7 +11,6 @@ from preprocessing.audio_processing import preprocess_audio
 from utils.record_audio import record_audio
 from utils.helpers import save_transcription
 
-# Function to return the correct model based on the type
 def get_model(model_type, model_name):
     if model_type == "wav2vec2":
         return Wav2Vec2SpeechRecognizer(model_name)
@@ -25,14 +24,12 @@ def get_model(model_type, model_name):
 def main(audio_path=None, use_mic=False):
     config = load_config()
 
-    # Step 1: Get audio input
     if use_mic:
         print("Recording from microphone...")
-        # Record audio, save in file, then preprocess with that file
         recorded_file = record_audio(
             filename="mic_recording.wav",
-            duration=config.get("record_duration", 5),             # Recording duration (default 5 sec)
-            fs=config.get("sample_rate", 16000),                   # Sampling rate (default 16 kHz)
+            duration=config.get("record_duration", 5),            
+            fs=config.get("sample_rate", 16000),                
             folder="audio_samples/mic_recordings" 
         )
         audio, sr = preprocess_audio(recorded_file)
@@ -41,28 +38,22 @@ def main(audio_path=None, use_mic=False):
     else:
         raise ValueError("Either --audio or --mic must be provided.")
 
-    # Step 2: Loading selected model
     recognizer = get_model(config["model_type"], config["model_name"])
 
-    # Step 3: Transcribing audio
     print("Transcribing audio...")
     transcription = recognizer.transcribe(audio, sr)
 
-    # Step 4: Display output
     print("Transcription Result:")
     print(transcription)
 
-    # Step 5: Save output to file
     if config.get("save_transcript", False):
         output_path = config.get("output_text_file", "transcript.txt")
         save_transcription(transcription, output_path)
 
-# It runs program by taking input from user (audio file or mic)
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Speech to Text Transcription")
     parser.add_argument('--audio', type=str, help="Path to audio file")
     parser.add_argument('--mic', action='store_true', help="Use microphone for input")
     args = parser.parse_args()
 
-    # Execute main function with arguments
     main(audio_path=args.audio, use_mic=args.mic)
