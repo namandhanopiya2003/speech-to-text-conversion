e7cda38ed10c35f5a02b1b4bd23df60b45449356
Naman Dhanopiya <iamnamandhanopiya@gmail.com>
2025-07-26 20:51:48 +0530

    Improved code readability

--- speech_to_text/config/config.py
+++ speech_to_text/config/config.py
@@ -1,13 +1,15 @@
 import yaml
 import os
 
-# Loads settings from a YAML file
+# It opens the config.yaml file and turns its content into a Python readable dictionary
 def load_config(config_path=None):
     if config_path is None:
-        # Default path is set to config.yaml inside the same folder
+        
+        # If no path is given, it uses the default "config.yaml"
         config_path = os.path.join(os.path.dirname(__file__), "config.yaml")
 
-    # Open the YAML file, read its content, and return it as a Python dictionary
+    # It opens the config.yaml file and read its content
     with open(config_path, "r") as f:
-        config = yaml.safe_load(f)
+        config = yaml.safe_load(f)             # Converts the YAML content into a Python dictionary (key-value pairs)
+        
     return config
