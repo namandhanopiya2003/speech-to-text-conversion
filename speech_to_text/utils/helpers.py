cba2048e9bd840ef279454a160de51e3c17d46e1
Naman Dhanopiya <iamnamandhanopiya@gmail.com>
2025-07-28 18:30:03 +0530

    Added comments

--- speech_to_text/utils/helpers.py
+++ speech_to_text/utils/helpers.py
@@ -1,6 +1,11 @@
 import os
 
+# The function saves the transcribed text in a file at the given location
 def save_transcription(text, output_path):
     with open(output_path, "w", encoding="utf-8") as f:
+
+        # Writes the text into the file
         f.write(text)
+
+    # Prints that the file was saved with path
     print(f"Transcript saved to {output_path}")
