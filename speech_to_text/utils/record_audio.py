7dbf331a163757d9977d2722684b10f12dad6e09
Naman Dhanopiya <iamnamandhanopiya@gmail.com>
2025-07-29 18:28:44 +0530

    Improved code readability

--- speech_to_text/utils/record_audio.py
+++ speech_to_text/utils/record_audio.py
@@ -4,10 +4,20 @@ import os
 
 def record_audio(filename="mic_recording.wav", duration=5, fs=16000, folder="audio_samples/mic_recordings"):
     print(f"Recording for {duration} seconds...")
+
+    # Records audio from microphone
     audio = sd.rec(int(duration * fs), samplerate=fs, channels=1, dtype='int16')
     sd.wait()
+
+    # Creates folder if it doesn't exist
     os.makedirs(folder, exist_ok=True)
+
+    # Creates full path for saving
     filepath = os.path.join(folder, filename)
+
+    # Saves recorded audio to WAV file
     write(filepath, fs, audio)
+    
     print(f"Saved recording: {filepath}")
+    # Returns path to the saved audio
     return filepath
